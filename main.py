9e4d5589e064b2987473d0fa468a21d779bee06e
Krtayu <lovelylayla206+krtnu@gmail.com>
2025-05-12 00:19:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ client = genai.Client(api_key=api_key)
 chat = client.chats.create(model="gemini-2.0-flash")
 
 app = CTk()
-app.geometry("500x600")
+app.geometry("960x550")
 app.title("Gemini AI Chat")
 
 chat_history = []
@@ -29,17 +29,13 @@ def send_message():
 
     text_area.insert(END, f"You: {user_text}\n", "user")
     text_area.insert(END, f"AI: {ai_response}\n", "ai")
-    text_area.insert(END, "-"*40 + "\n")
+    text_area.insert(END, "-"*40 + "\n" + "\n")
 
-text_area = CTkTextbox(app, wrap="word", width=480, height=450, font=("Arial", 14))
+text_area = CTkTextbox(app, wrap="word", width=920, height=450, font=("Roboto", 16))
 text_area.pack(pady=10)
 
-entry = CTkEntry(app, width=400, font=("Arial", 14))
+entry = CTkEntry(app, width=400, font=("Roboto", 14))
 entry.pack(pady=10)
 entry.bind("<Return>", lambda event: send_message())  
-
-send_button = CTkButton(app, text="Send", command=send_message)
-send_button.pack(pady=10)
-
 
 app.mainloop()
